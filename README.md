c48174aa042c7b43feabe1c72bec825f8a3efa00
Andy Clark <andy.clark@build.com>
2019-03-03 23:13:23 -0800

    Write example.png

---
+++ README.md
@@ -0,0 +1,20 @@
+# pipupdash
+
+## Environment Variables
+* `HEADLESS` - Defaults to `false`
+* `SLOWMO` - Optional value in ms between characters typed into a form field
+* `VIEWPORT_WIDTH` - Defaults to 1920
+* `VIEWPORT_HEIGHT` - Defaults to 1080
+* `REPEAT` - Number of times to repeat. Defaults to 0. A value of `-2` will repeat indefinitely.
+* `SITES` - Required list of sites in JSON
+  * `url` - Required URL
+  * `wait` - Optional value in ms to wait after the site is loaded
+  * `login` - Optional log in information.
+    * `usernameSelector`
+    * `passwordSelector`
+    * `username`
+    * `password`
+
+## Running
+
+        SITES='[{"url":"https://www.google.com/"},{"url":"https://slashdot.org/","wait":2000},{"url":"http://mysensuserver.mydomain:3000/","login":{"url":"http://mysensuserver.mydomain:3000/","usernameSelector":"input[name=\"username\"]","passwordSelector":"input[name=\"pass\"]","username":"adminuser","password":"adminpassword"}}]' node index.js
--- index.js
+++ index.js
@@ -1,7 +1,59 @@
-const configFilepath = './config.yaml';
-const config = require('./config').readConfig(configFilepath);
+const puppeteer = require('puppeteer');
 
-Object.keys(config.pages).forEach((key) => {
-  console.log(key);
-  console.log(config.pages[key]);
-});
+const getSites = () => {
+  if (!('SITES' in process.env)) {
+    throw new Error('SITES is missing from the environment')
+  }
+  return JSON.parse(process.env['SITES']);
+};
+
+const getOptions = () => {
+  return {
+    browser: {
+      headless: 'HEADLESS' in process.env ? process.env['HEADLESS'] === 'true' : false,
+      slowMo: 'SLOWMO' in process.env ? parseInt(process.env['SLOWMO']) : 0,
+    },
+    viewport: {
+      width: 'VIEWPORT_WIDTH' in process.env ? parseInt(process.env['VIEWPORT_WIDTH']) : 1920,
+      height: 'VIEWPORT_HEIGHT' in process.env ? parseInt(process.env['VIEWPORT_HEIGHT']) : 1080
+    },
+    repeat: 'REPEAT' in process.env ? parseInt(process.env['REPEAT']) : 0
+  };
+};
+
+const doSites = async (sites, options) => {
+  const browser = await puppeteer.launch(options.browser);
+  var repeatCount = options.repeat;
+
+  while (repeatCount >= 0) {
+    for (const site of sites) {
+      await doSite(site, browser, options);
+    }
+    if (repeatCount > -2) { // repeatCount of -2 will repeat indefinitely
+      repeatCount--;
+    }
+  }
+  await browser.close();
+  console.log('done');
+};
+
+const doSite = async (site, browser, options) => {
+  const page = await browser.newPage();
+  await page.setViewport(options.viewport);
+  await page.goto(site['url']);
+  if ('login' in site) { // LATER validate site options
+    if ('usernameSelector' in site['login'] && 'username' in site['login']) {
+      await page.$eval(site['login']['usernameSelector'], input => input.value = '');
+      await page.type(site['login']['usernameSelector'], site['login']['username']);
+    }
+    if ('passwordSelector' in site['login'] && 'password' in site['login']) {
+      await page.$eval(site['login']['passwordSelector'], input => input.value = '');
+      await page.type(site['login']['passwordSelector'], site['login']['password'] + '\n');
+    }
+  }
+  await page.waitFor('wait' in site ? parseInt(site['wait']) : 1000);
+  await page.screenshot({path: 'example.png'});
+  await page.close();
+};
+
+doSites(getSites(), getOptions());
