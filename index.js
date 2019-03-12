c1ae68f2dafcbbc70e3916c5f56ad31b39ce67b2
Andy Clark <andy.clark@build.com>
2019-03-11 21:35:18 -0700

    VSCode debugging
    Fix environment variable default logic

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ const getOptions = () => {
     browser: {
       args: [
         '--disable-setuid-sandbox',
-        // '--disable-dev-shm-usage',
-        // '--shm-size="1gb"',
         '--disable-gpu',
         '--no-sandbox'
       ],
-      headless: 'HEADLESS' in process.env && process.env['HEADLESS'] !== 'false',
+      headless: 'HEADLESS' in process.env ? process.env['HEADLESS'] !== 'true' : true,
       slowMo: 'SLOWMO' in process.env ? parseInt(process.env['SLOWMO']) : 0,
       executablePath: '/usr/bin/google-chrome'
     },
